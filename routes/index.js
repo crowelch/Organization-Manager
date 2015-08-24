7137dc5294442423d8fc7298bb167683922d0f85
Chris Crowell <crowelch@mail.uc.edu>
2015-08-24 00:24:27 -0400

    Add create account get and post routes, attendance get and post routes, login route, and verifyCard dummy function for use in attendance.

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,43 @@
 var express = require('express');
 var router = express.Router();
-var controller = require('../controllers/index');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* POST home page. */
-router.post('/', function(req, res, next) {
-	console.dir(req.body);
-	res.send(JSON.stringify(req.body));
+router.get('/create-account', function(req, res, next) {
+	res.render('create_account');
 });
+
+router.post('/create-account', function(req, res, next) {
+	console.dir(req.body);
+	res.render('account_created');
+});
+
+router.get('/attendance', function(req, res, next) {
+	if(req.body.login) {
+		req.flash('success', {
+			msg: 'Good job!'
+		});
+		req.body.login = false;
+	} else {
+		res.render('attendance');
+	}
+});
+
+router.post('/attendance', function(req, res, next) {
+	console.log(req.body.card);
+
+});
+
+router.get('/login', function(req, res, next) {
+	res.render('login');
+});
+
+function verifyCard(card) {
+	//if card in db, sign in
+	//otherwise throw err
+}
 
 module.exports = router;
