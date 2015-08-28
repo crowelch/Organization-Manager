3835e00d17c6ff1eb2178ecff2062e810f45946b
Chris Crowell <crowelch@mail.uc.edu>
2015-08-28 14:51:40 -0400

    work on sql stufff

--- utils/sql.js
+++ utils/sql.js
@@ -7,6 +7,7 @@ var params = require('../config/secrets.js').params;
 
 
 exports.putUser = function(userObject) {
+	console.dir(userObject);
 	return new Promise(function(resolve, reject) {
 		console.log(userObject);
 		hashed(userObject.card).then(function(result) {
@@ -24,7 +25,7 @@ exports.putUser = function(userObject) {
 			});
 			connection.end(function(err) {
 				if(err) {
-					reject(err);
+					console.log(err);
 				}
 			});
 
@@ -35,51 +36,39 @@ exports.putUser = function(userObject) {
 }
 
 exports.signIn = function(card) {
-	//else insert meeting key and userId into attendance table
-	//display success
+	return new Promise(function(resolve, reject) {
+		var meetingKey;
+		var userKey;
 
-	var meetingKey;
-	var userKey;
-	var today = mysql.escape(moment().format('YYYY-MM-DD'));
-	console.log(typeof today);
-
-	var connection = mysql.createConnection(params);
-	connection.connect();
-
-	// Check that a meeting is happening today
-	new Promise(function(resolve, reject) {
-		connection.query("SELECT * FROM meetings WHERE date=" + today, function(err, result) {
-			if(err) {
-				reject(err);
-			}
-			console.log('meeting?', result);
-			resolve(result[0].meetingKey);
-		});
-	// Assign meetingKey
-	}).then(function(result) {
-		if(result === []) {
-			console.log('no meeting');
-		}
-
-		meetingKey = result
-		console.log('meetingKey:', meetingKey);
-	}, function(error) {
-		console.log(error);
-	// Find user
-	}).then(function(result) {
-		connection.query("SELECT * FROM members", function(err, result) {
-			if(err) {
-				console.log('err', err);
+		// Check that a meeting is happening today
+		checkMeeting().then(function(result) {
+			if(result === []) {
+				reject('No meeting today you scurvy curr!');
 			}
 
-			hashCompare(result, card).then(function(result) {
-				var membersKey = result;
-				console.log('membersKey', membersKey);
+			meetingKey = result
+		}, function(error) {
+			reject(error);
+		}).then(function() {
+			console.log('ji');
+			hashCompare(card).then(function(result) {
+
+				var memberKey = result;
+				if(memberkey === undefined) {
+					reject('Member not found, have you created an account?');
+				}
+				console.log('memberKey', memberKey);
+
+				var connection = mysql.createConnection(params);
+				connection.connect();
 				connection.query("INSERT INTO attendance SET ?", {
-					memberKey: membersKey,
+					memberKey: memberKey,
 					meetingKey: meetingKey
 				}, function(err, result) {
-					console.log('err in insertattnd', err);
+					if(err) {
+						console.log('err in insertattnd', err);
+					}
+					resolve(result);
 					console.log('attend?', result);
 
 					connection.end(function(err) {
@@ -88,13 +77,12 @@ exports.signIn = function(card) {
 						}
 					});
 				});
-			}, function(error) {
-				console.log(error);
 			});
+		}, function(error) {
+			console.log(error);
 		});
-	}, function(error) {
-		console.log(error);
 	});
+
 }
 
 exports.createMeeting = function(date) {
@@ -140,20 +128,55 @@ function hashed(data) {
 	});
 }
 
-function hashCompare(members, card) {
+function hashCompare(card) {
 	return new Promise(function(resolve, reject) {
-		_.forEach(members, function(member) {
-			console.log(card, member.card);
-			bcrypt.compare(card, member.card, function(err, same) {
-				if(err) {
-					console.log('compare err', err);
-				} else if(same) {
-					resolve(member.membersKey)
-				}
+
+		var connection = mysql.createConnection(params);
+		connection.connect();
+		connection.query("SELECT * FROM members", function(err, result) {
+			if(err) {
+				console.log('err', err);
+			}
+
+			_.forEach(result, function(member) {
+				console.log('caaaard',card, member.card);
+				bcrypt.compare(card, member.card, function(err, same) {
+					if(err) {
+						console.log('compare err', err);
+					} else if(same) {
+						console.log('samesies');
+						resolve(member.memberkey);
+					}
+				});
 			});
+
+			reject('ohno');
 		});
-		// reject('card not found');
+
+		connection.end(function(err) {
+			console.log('hashcardcannotcloseerrror', err);
+		});
 	});
+}
+
+function checkMeeting() {
+	return new Promise(function(resolve, reject) {
+		var today = mysql.escape(moment().format('YYYY-MM-DD'));
+		var connection = mysql.createConnection(params);
+		connection.connect();
+
+		connection.query("SELECT * FROM meetings WHERE date=" + today, function(err, result) {
+			if(err) {
+				reject(err);
+			}
+			console.log('meeting?', result);
+			resolve(result[0].meetingKey);
+		});
+
+		connection.end(function(err) {
+			console.log('hashcardcannotcloseerrror', err);
+		});
+	})
 }
 
 function dateCheck(date) {
