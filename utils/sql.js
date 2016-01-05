b326ba24f6e86b9fce78d8002ad8059d1bdaca2e
Chris Crowell <crowelch@mail.uc.edu>
2016-01-05 18:02:51 -0500

    Rename betterSignIn to mNumberSignIn and remove card parameter, and rename signIn to cardSignIn. Remove function dbEnterSignIn as it was rendered useless by the utils/db.js abstractions. Create verifyMNumber function to ensure there is a capital M at the beginning of the mNumber. Greatly clean up the promise chain in mNumberSignIn.

--- utils/sql.js
+++ utils/sql.js
@@ -19,93 +19,41 @@ exports.createMember = function(userObject) {
 	});
 };
 
-exports.betterSignIn = function(mnumber, card) {
-	//todo add m to number if missing, uppercase if there
+exports.mNumberSignIn = function(providedMNumber) {
 	return new Promise(function(resolve, reject) {
-		var meetingKey;
-		var userKey;
-		utils.checkMeeting().then(function(result) {
-			if(result === []) {
-				reject('No meeting today you scurvy curr!');
-			} else {
-				meetingKey = result;
-				return result;
-			}
+		var attendanceObject = {};
+
+		utils.checkMeetingIsToday().then(function(meetingKey) {
+			attendanceObject.meetingKey = meetingKey;
 		}, function(error) {
 			reject(error);
+			throw new Error('No Meeting');
 		}).then(function() {
-			if(mnumber) {
-				return utils.mNumberFinder(mnumber);
-			} else if(card) {
-				return utils.hashCompare(card);
-			} else {
-				reject('no card or mnumber provided');
-			}
-		}).then(function(userKey) {
-			console.log('back from mNumberFinder:', userKey);
-			if(meetingKey === undefined || userKey === undefined) {
-				console.log('ohno');
-				reject({
-					meetingError: {
-						databaseError: true
-					}
-				});
-			} else {
-				console.log('hi');
-				dbEnterSignIn(meetingKey, userKey).then(function(queryResponse) {
-					console.log('qr:', queryResponse);
+			return verifyMNumber(providedMNumber);
+		}).then(function(verifiedMNumber) {
+			return utils.memberLookupByMNumber(verifiedMNumber);
+		}).then(function(memberKey) {
+			attendanceObject.memberKey = memberKey;
+
+			db.insert('INSERT INTO attendance SET ?', attendanceObject)
+				.then(function(queryResponse) {
 					resolve('');
 				}, function(error) {
 					reject(error);
 				});
-			}
+
 		}, function(error) {
 			reject(error);
 		});
 	});
 };
 
-function dbEnterSignIn(meetingKey, userKey) {
-	console.log('enter meeting attendance...');
-	return new Promise(function(resolve, reject) {
-		var connection = mysql.createConnection(params);
-		connection.connect(function(err) {
-			console.log('no?', err);
-		});
-		connection.query("INSERT INTO attendance SET ?", {
-			memberKey: userKey,
-			meetingKey: meetingKey
-		}, function(err, result) {
-			if(err) {
-				if(err.code === 'ER_DUP_ENTRY') {
-					reject({
-						meetingError: {
-							alreadySignedIn: true
-						}
-					});
-				}
-				console.log('err in insertattnd', err);
-			} else {
-				console.log('attend?', result);
-				resolve(result);
-			}
-
-			connection.end(function(err) {
-				if(err) {
-					console.log(err);
-				}
-			});
-		});
-	});
-}
-
-exports.signIn = function(card) {
+exports.cardSignIn = function(card) {
 	return new Promise(function(resolve, reject) {
 		var meetingKey;
 		var userKey;
 		// Check that a meeting is happening today
 		utils.checkMeeting().then(function(result) {
-			console.log('after checkmeeting');
 			if(result === []) {
 				reject('No meeting today you scurvy curr!');
 			}
@@ -113,59 +61,13 @@ exports.signIn = function(card) {
 		}, function(error) {
 			reject(error);
 		}).then(function() {
-			console.log('about to check member');
-
 			utils.mNumberFinder(card).then(function(result){
 				console.log('hi');
 			}, function(error) {
 				console.log(error);
 			});
-
-			// utils.hashCompare(card).then(function(result) {
-			// 	var memberKey = result;
-			// 	if(memberKey === undefined) {
-			// 		reject('Member not found, have you created an account?');
-			// 	} else {
-			// 		console.log('memberKey', memberKey);
-
-			// 		var connection = mysql.createConnection(params);
-			// 		connection.connect();
-			// 		connection.query("INSERT INTO attendance SET ?", {
-			// 			memberKey: memberKey,
-			// 			meetingKey: meetingKey
-			// 		}, function(err, result) {
-			// 			if(err) {
-			// 				if(err.code ==='ER_DUP_ENTRY') {
-			// 					reject({
-			// 						meetingError: {
-			// 							alreadySignedIn: true
-			// 						}
-			// 					});
-			// 					return;
-			// 				}
-			// 				console.log('err in insertattnd', err);
-			// 			} else {
-			// 				resolve(result);
-			// 				console.log('attend?', result);
-			// 			}
-
-			// 			connection.end(function(err) {
-			// 				if(err) {
-			// 					console.log(err);
-			// 				}
-			// 			});
-			// 		});
-			// 	}
-			// }, function(error) {
-			// 	reject({
-			// 		meetingError: {
-			// 			noAccount: true
-			// 		}
-			// 	});
-			// 	console.log('eyyyy:', error);
-			// });
 		}, function(error) {
-			console.log('y' + error);
+			console.log('signInError: ' + error);
 		});
 	});
 };
@@ -193,6 +95,10 @@ exports.getMembersEmails = function() {
 	});
 };
 
+exports.getMeetings = function() {
+	return db.select('SELECT * FROM meetings');
+};
+
 function emailsJSONToCSV(emailsJSON) {
 	var LAST_COMMA_POSITION = emailsJSON.length - 1;
 	var emailsCSV = "";
@@ -208,6 +114,14 @@ function emailsJSONToCSV(emailsJSON) {
 	return emailsCSV;
 }
 
-exports.getMeetings = function() {
-	return db.select('SELECT * FROM meetings');
-};
+function verifyMNumber(mNumber) {
+	if(mNumber.charAt(0) === 'm') {
+		mNumber = mNumber.replace('m', 'M');
+	}
+
+	if(mNumber.charAt(0).toUpperCase() !== 'M') {
+		mNumber = 'M' + mNumber;
+	}
+
+	return Promise.resolve(mNumber);
+}
